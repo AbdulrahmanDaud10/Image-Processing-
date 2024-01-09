9952735d94fd6b1dc6188d123c6b2ef6f64233de
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2024-01-09 12:16:45 +0300

    Redis Connection Implementation

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"log"
 
+	"github.com/AbdulrahmanDaud10/image-processing-golang-service/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -10,5 +11,6 @@ const redisAddress = "127.0.0.1:6379"
 
 func main() {
 	app := fiber.New()
+	routes.RouteSetup(app)
 	log.Fatal(app.Listen(":3000"))
 }
