1b0550a2bc645cf5972e972319238e2bbc10f197
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2024-01-09 11:36:59 +0300

    Handlers Import Bug fix

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,9 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/AbdulrahmanDaud10/image-processing-golang-service/handlers"
+	"github.com/gofiber/fiber/v2"
+)
 
 func RouteSetup(app *fiber.App) {
 	app.Post("/image-process", handlers.UploadImage)
